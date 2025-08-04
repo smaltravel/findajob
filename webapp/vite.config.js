ecc4b150d711bd8aff4d5df7b141d6dfa817b5e7
Viacheslav <smaltravel@gmail.com>
2025-08-04 16:25:46 +0200

    updated webapp to use tailwindcss

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
   preview: {
     port: 8080,
     strictPort: true,
