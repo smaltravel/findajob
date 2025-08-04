f0c0c7a35250bb4e76273739e91675cba69c8da2
Viacheslav <smaltravel@gmail.com>
2025-08-04 16:19:50 +0200

    initialize vite web app

---
+++ webapp/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/",
+  plugins: [vue()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  }
+})
